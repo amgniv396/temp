a2f31e598a522c43e8bbd4abf75dd243fa4b1268
eyal sharabi <eyal.shara@gmail.com>
2026-01-14 22:12:01 +0200

    simple pid controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/JeruLib/PIDController/PIDController.java
@@ -0,0 +1,413 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.firstinspires.ftc.teamcode.Libraries.JeruLib.PIDController;
+
+import org.firstinspires.ftc.teamcode.Libraries.JeruLib.Utils.mathUtils;
+
+/** Implements a PID control loop. */
+public class PIDController {
+
+    // Factor for "proportional" control
+    private double m_kp;
+
+    // Factor for "integral" control
+    private double m_ki;
+
+    // Factor for "derivative" control
+    private double m_kd;
+
+    // The error range where "integral" control applies
+    private double m_iZone = Double.POSITIVE_INFINITY;
+
+    // The period (in seconds) of the loop that calls the controller
+    private final double m_period;
+
+    private double m_maximumIntegral = 1.0;
+
+    private double m_minimumIntegral = -1.0;
+
+    private double m_maximumInput;
+
+    private double m_minimumInput;
+
+    // Do the endpoints wrap around? e.g. Absolute encoder
+    private boolean m_continuous;
+
+    // The error at the time of the most recent call to calculate()
+    private double m_currentError;
+    private double m_errorRate;
+
+    // The error at the time of the second-most-recent call to calculate() (used to compute error rate)
+    private double m_prevError;
+
+    // The sum of the errors for use in the integral calc
+    private double m_totalError;
+
+    // The error that is considered at setpoint.
+    private double m_errorTolerance = 0.05;
+    private double m_errorRateTolerance = Double.POSITIVE_INFINITY;
+
+    private double m_setpoint;
+    private double m_measurement;
+
+    private boolean m_haveMeasurement;
+    private boolean m_haveSetpoint;
+
+    /**
+     * Allocates a PIDController with the given constants for kp, ki, and kd and a default period of
+     * 0.02 seconds.
+     *
+     * @param kp The proportional coefficient.
+     * @param ki The integral coefficient.
+     * @param kd The derivative coefficient.
+     * @throws IllegalArgumentException if kp &lt; 0
+     * @throws IllegalArgumentException if ki &lt; 0
+     * @throws IllegalArgumentException if kd &lt; 0
+     */
+    public PIDController(double kp, double ki, double kd) {
+        this(kp, ki, kd, 0.02);
+    }
+
+    /**
+     * Allocates a PIDController with the given constants for kp, ki, and kd.
+     *
+     * @param kp The proportional coefficient.
+     * @param ki The integral coefficient.
+     * @param kd The derivative coefficient.
+     * @param period The period between controller updates in seconds.
+     * @throws IllegalArgumentException if kp &lt; 0
+     * @throws IllegalArgumentException if ki &lt; 0
+     * @throws IllegalArgumentException if kd &lt; 0
+     * @throws IllegalArgumentException if period &lt;= 0
+     */
+    @SuppressWarnings("this-escape")
+    public PIDController(double kp, double ki, double kd, double period) {
+        setPID(kp,ki,kd);
+
+        if (period <= 0.0) {
+            throw new IllegalArgumentException("Controller period must be a positive number!");
+        }
+        m_period = period;
+    }
+
+
+    /**
+     * Sets the PID Controller gain parameters.
+     *
+     * <p>Set the proportional, integral, and differential coefficients.
+     *
+     * @param kp The proportional coefficient.
+     * @param ki The integral coefficient.
+     * @param kd The derivative coefficient.
+     */
+    public void setPID(double kp, double ki, double kd) {
+        setP(kp);
+        setI(ki);
+        setD(kd);
+    }
+
+    /**
+     * Sets the Proportional coefficient of the PID controller gain.
+     *
+     * @param kp The proportional coefficient. Must be &gt;= 0.
+     */
+    public void setP(double kp) {
+        if (kp < 0.0) {
+            throw new IllegalArgumentException("Kp must be a non-negative number!");
+        }
+        m_kp = kp;
+    }
+
+    /**
+     * Sets the Integral coefficient of the PID controller gain.
+     *
+     * @param ki The integral coefficient. Must be &gt;= 0.
+     */
+    public void setI(double ki) {
+        if (ki < 0.0) {
+            throw new IllegalArgumentException("Ki must be a non-negative number!");
+        }
+        m_ki = ki;
+    }
+
+    /**
+     * Sets the Differential coefficient of the PID controller gain.
+     *
+     * @param kd The differential coefficient. Must be &gt;= 0.
+     */
+    public void setD(double kd) {
+        if (kd < 0.0) {
+            throw new IllegalArgumentException("Kd must be a non-negative number!");
+        }
+        m_kd = kd;
+    }
+
+    /**
+     * Sets the IZone range. When the absolute value of the error is greater than IZone, the
+     * total accumulated error will reset to zero, disabling integral gain until the absolute value of
+     * the error is less than IZone. This is used to prevent integral windup. Must be
+     * non-negative. Passing a value of zero will effectively disable integral gain. Passing a value
+     * of {@link Double#POSITIVE_INFINITY} disables IZone functionality.
+     *
+     * @param iZone Maximum magnitude of error to allow integral control.
+     * @throws IllegalArgumentException if iZone &lt; 0
+     */
+    public void setIZone(double iZone) {
+        if (iZone < 0) {
+            throw new IllegalArgumentException("IZone must be a non-negative number!");
+        }
+        m_iZone = iZone;
+    }
+
+    /**
+     * Get the Proportional coefficient.
+     *
+     * @return proportional coefficient
+     */
+    public double getP() {
+        return m_kp;
+    }
+
+    /**
+     * Get the Integral coefficient.
+     *
+     * @return integral coefficient
+     */
+    public double getI() {
+        return m_ki;
+    }
+
+    /**
+     * Get the Differential coefficient.
+     *
+     * @return differential coefficient
+     */
+    public double getD() {
+        return m_kd;
+    }
+
+    /**
+     * Get the IZone range.
+     *
+     * @return Maximum magnitude of error to allow integral control.
+     */
+    public double getIZone() {
+        return m_iZone;
+    }
+
+    /**
+     * Returns the period of this controller.
+     *
+     * @return the period of the controller.
+     */
+    public double getPeriod() {
+        return m_period;
+    }
+
+    /**
+     * Returns the error tolerance of this controller.
+     *
+     * @return the error tolerance of the controller.
+     */
+    public double getErrorTolerance() {
+        return m_errorTolerance;
+    }
+
+    /**
+     * Returns the error rate tolerance of this controller.
+     *
+     * @return the error rate tolerance of the controller.
+     */
+    public double getErrorRateTolerance() {
+        return m_errorRateTolerance;
+    }
+
+    /**
+     * Sets the setpoint for the PIDController.
+     *
+     * @param setpoint The desired setpoint.
+     */
+    public void setSetpoint(double setpoint) {
+        m_setpoint = setpoint;
+        m_haveSetpoint = true;
+
+        if (m_continuous) {
+            double errorBound = (m_maximumInput - m_minimumInput) / 2.0;
+            m_currentError = mathUtils.inputModulus(m_setpoint - m_measurement, -errorBound, errorBound);
+        } else {
+            m_currentError = m_setpoint - m_measurement;
+        }
+
+        m_errorRate = (m_currentError - m_prevError) / m_period;
+    }
+
+    /**
+     * Returns the current setpoint of the PIDController.
+     *
+     * @return The current setpoint.
+     */
+    public double getSetpoint() {
+        return m_setpoint;
+    }
+
+    /**
+     * Returns true if the error is within the tolerance of the setpoint.
+     *
+     * <p>This will return false until at least one input value has been computed.
+     *
+     * @return Whether the error is within the acceptable bounds.
+     */
+    public boolean atSetpoint() {
+        return m_haveMeasurement
+                && m_haveSetpoint
+                && Math.abs(m_currentError) < m_errorTolerance
+                && Math.abs(m_errorRate) < m_errorRateTolerance;
+    }
+
+    /**
+     * Enables continuous input.
+     *
+     * <p>Rather then using the max and min input range as constraints, it considers them to be the
+     * same point and automatically calculates the shortest route to the setpoint.
+     *
+     * @param minimumInput The minimum value expected from the input.
+     * @param maximumInput The maximum value expected from the input.
+     */
+    public void enableContinuousInput(double minimumInput, double maximumInput) {
+        m_continuous = true;
+        m_minimumInput = minimumInput;
+        m_maximumInput = maximumInput;
+    }
+
+    /** Disables continuous input. */
+    public void disableContinuousInput() {
+        m_continuous = false;
+    }
+
+    /**
+     * Returns true if continuous input is enabled.
+     *
+     * @return True if continuous input is enabled.
+     */
+    public boolean isContinuousInputEnabled() {
+        return m_continuous;
+    }
+
+    /**
+     * Sets the minimum and maximum values for the integrator.
+     *
+     * <p>When the cap is reached, the integrator value is added to the controller output rather than
+     * the integrator value times the integral gain.
+     *
+     * @param minimumIntegral The minimum value of the integrator.
+     * @param maximumIntegral The maximum value of the integrator.
+     */
+    public void setIntegratorRange(double minimumIntegral, double maximumIntegral) {
+        m_minimumIntegral = minimumIntegral;
+        m_maximumIntegral = maximumIntegral;
+    }
+
+    /**
+     * Sets the error which is considered tolerable for use with atSetpoint().
+     *
+     * @param errorTolerance error which is tolerable.
+     */
+    public void setTolerance(double errorTolerance) {
+        setTolerance(errorTolerance, Double.POSITIVE_INFINITY);
+    }
+
+    /**
+     * Sets the error which is considered tolerable for use with atSetpoint().
+     *
+     * @param errorTolerance error which is tolerable.
+     * @param errorRateTolerance error Rate which is tolerable.
+     */
+    public void setTolerance(double errorTolerance, double errorRateTolerance) {
+        m_errorTolerance = errorTolerance;
+        m_errorRateTolerance = errorRateTolerance;
+    }
+
+    /**
+     * Returns the difference between the setpoint and the measurement.
+     *
+     * @return The error.
+     */
+    public double getError() {
+        return m_currentError;
+    }
+
+    /**
+     * Returns the error rate.
+     *
+     * @return The error rate.
+     */
+    public double getErrorRate() {
+        return m_errorRate;
+    }
+
+    public double getMinimumIntegral(){
+        return m_minimumIntegral;
+    }
+
+    public double getMaximumIntegral() {
+        return m_maximumIntegral;
+    }
+
+    /**
+     * Returns the next output of the PID controller.
+     *
+     * @param measurement The current measurement of the process variable.
+     * @param setpoint The new setpoint of the controller.
+     * @return The next controller output.
+     */
+    public double calculate(double measurement, double setpoint) {
+        m_setpoint = setpoint;
+        m_haveSetpoint = true;
+        return calculate(measurement);
+    }
+
+    /**
+     * Returns the next output of the PID controller.
+     *
+     * @param measurement The current measurement of the process variable.
+     * @return The next controller output.
+     */
+    public double calculate(double measurement) {
+        m_measurement = measurement;
+        m_prevError = m_currentError;
+        m_haveMeasurement = true;
+
+        if (m_continuous) {
+            double errorBound = (m_maximumInput - m_minimumInput) / 2.0;
+            m_currentError = mathUtils.inputModulus(m_setpoint - m_measurement, -errorBound, errorBound);
+        } else {
+            m_currentError = m_setpoint - m_measurement;
+        }
+
+        m_errorRate = (m_currentError - m_prevError) / m_period;
+
+        // If the absolute value of the error is greater than IZone, reset the total error
+        if (Math.abs(m_currentError) > m_iZone) {
+            m_totalError = 0;
+        } else if (m_ki != 0) {
+            m_totalError =
+                    mathUtils.clamp(
+                            m_totalError + m_currentError * m_period,
+                            m_minimumIntegral / m_ki,
+                            m_maximumIntegral / m_ki);
+        }
+
+        return m_kp * m_currentError + m_ki * m_totalError + m_kd * m_errorRate;
+    }
+
+    /** Resets the previous error and the integral term. */
+    public void reset() {
+        m_currentError = 0;
+        m_prevError = 0;
+        m_totalError = 0;
+        m_errorRate = 0;
+        m_haveMeasurement = false;
+    }
+}
