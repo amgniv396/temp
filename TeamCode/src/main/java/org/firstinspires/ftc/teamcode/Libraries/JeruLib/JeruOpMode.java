5553f7783424880f209813e2ae5e97f1d9f36ca0
eyal sharabi <eyal.shara@gmail.com>
2026-01-22 14:56:23 +0200

    i think it is field overlay for dashboard, need to check

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/JeruLib/JeruOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/JeruLib/JeruOpMode.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.Libraries.JeruLib;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 
 import org.firstinspires.ftc.teamcode.Libraries.JeruLib.Utils.OpModeType;
+import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.Drawing;
 
 public abstract class JeruOpMode extends CommandOpMode {
     @Override
@@ -18,9 +20,13 @@ public abstract class JeruOpMode extends CommandOpMode {
         JeruRobot.getInstance().localizer.update();
         telemetry.addData("a", Math.toDegrees(JeruRobot.getInstance().localizer.getPositionRR().heading.toDouble()));
 
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.fieldOverlay().setStroke("#3F51B5");
+        Drawing.drawRobot(packet.fieldOverlay(), JeruRobot.getInstance().localizer.getPositionRR());
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
         telemetry.update();
         FtcDashboard.getInstance().getTelemetry().update();
-
     }
     @Override
     public void end() {
